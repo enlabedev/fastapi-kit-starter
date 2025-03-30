30c985eec62245d681b868f8c135a587e34ca1ed
Enrique Lazo Bello <enlabe@gmail.com>
2025-03-30 09:08:30 -0500

    Refactor database session handling to use sessionmaker directly and update type hints for get_db function

--- app/config/database.py
+++ app/config/database.py
@@ -1,6 +1,6 @@
 from typing import Generator
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm import sessionmaker, Session
 from app.config.settings import settings
 
 
@@ -8,12 +8,10 @@ SQLITE_URL = settings.SQLITE_URL
 
 
 engine = create_engine(SQLITE_URL, connect_args={"check_same_thread": False}, echo=True)
-SessionLocal = scoped_session(
-    sessionmaker(autocommit=False, autoflush=False, bind=engine)
-)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-def get_db() -> Generator:
+def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
         yield db
