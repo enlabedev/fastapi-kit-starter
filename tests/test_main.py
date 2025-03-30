5c76ff6b09185c035fb63fffdef90ef55be8fc38
Enrique Lazo Bello <enlabe@gmail.com>
2025-03-30 17:57:26 -0500

    Refactor test_health function to improve type hinting

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ from fastapi.testclient import TestClient
 client = TestClient(app)
 
 
-def test_health():
+def test_health() -> None:
     response = client.get("/api/healthchecker")
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to FastAPI with SQLAlchemy"}
